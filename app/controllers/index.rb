5a5cb91b653f90cc42d0d7a548b3feefab6e2dd7
Fung Wei Tan Tai <fungwei@goget.my>
2014-11-06 17:53:49 +0800

    added ajax spinner =D

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,16 +2,31 @@ get '/' do
 end
 
 get '/:username' do
-    user = TwitterUser.find_by_username(params[:username])
-  if (user.tweets.empty? || user.tweets_stale?)
+  # @user = TwitterUser.find_by_username(params[:username])
+  # if (@user.tweets.empty? || @user.tweets_stale?)
+  #   # User#fetch_tweets! should make an API call
+  #   # and populate the tweets table
+  #   #
+  #   # Future requests should read from the tweets table
+  #   # instead of making an API call
+  #   @user.fetch_tweets!
+  # end
+
+  # @tweets = @user.tweets
+  erb :index
+end
+
+get '/:username/refresh' do
+  @user = TwitterUser.find_by_username(params[:username])
+  if (@user.tweets.empty? || @user.tweets_stale?)
     # User#fetch_tweets! should make an API call
     # and populate the tweets table
     #
     # Future requests should read from the tweets table
     # instead of making an API call
-    user.fetch_tweets!
+    @user.fetch_tweets!
   end
 
-  @tweets = user.tweets
-  erb :index
+  @tweets = @user.tweets
+  erb :tweets, :layout => false
 end
